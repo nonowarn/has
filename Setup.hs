94f5d1ef31910e4ba720af7b63068d1cbe20215b
nonowarn <nonowarn@gmail.com>
2019-04-23 17:47:16 +0900

    Fix testsuite

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,4 @@
 #!/usr/bin/env runhaskell
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import System.Process
-import System.FilePath
 
-main = defaultMainWithHooks hooks
-  where
-    hooks = simpleUserHooks { runTests = runTests' }
-
-runTests' _ _ _ lbi = system testprog >> return ()
-  where
-    testprog = (buildDir lbi) </> "test" </> "test"
+main = defaultMain
